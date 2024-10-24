1c81c762481877738568650c72c874b3cb316e88
Vasile Crăciunescu <vasile.craciunescu@gmail.com>
2024-10-24 11:03:02 +0300

    misc

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -82,7 +82,7 @@ const config = {
             type: "docSidebar",
             sidebarId: "tutorialSidebar",
             position: "left",
-            label: "Tutorial",
+            label: "Tutoriale",
           },
           { to: "/blog", label: "Blog", position: "left" },
           {
