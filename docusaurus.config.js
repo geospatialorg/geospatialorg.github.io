c11e511e67cb75e744497558d05ad7b26519b6b8
Vasile Crăciunescu <vasile.craciunescu@gmail.com>
2025-04-07 20:27:09 +0300

    “update”

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,8 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://geospatialorg.github.io",
+  //url: "https://geospatialorg.github.io",
+  url: "https://geo-spatial.org",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
