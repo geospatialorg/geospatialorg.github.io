7dc4216acf4d61a551467166066ce8475e7fb8a4
Vasile Crăciunescu <vasile.craciunescu@gmail.com>
2025-07-06 12:59:57 +0300

    Adăugare ghid județe poligon

--- sidebarsGhiduri.js
+++ sidebarsGhiduri.js
@@ -26,6 +26,7 @@ export default {
               items: [
                 "procesari-etl/administrative-boundaries/ro-admin-lau-polygon",
                 "procesari-etl/administrative-boundaries/ro-admin-lau-line",
+                "procesari-etl/administrative-boundaries/ro-admin-county-polygon",
                 "procesari-etl/administrative-boundaries/ro-admin-lau-simplified-polygon",
                 "procesari-etl/administrative-boundaries/ro-admin-lau-simplified-line",
                 "procesari-etl/administrative-boundaries/ro-admin-lau-bucharest-merged-polygon",
