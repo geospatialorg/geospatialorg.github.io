aced67efdef6a1b3a15f1c69ae19586519291dea
Vasile Crăciunescu <vasile.craciunescu@gmail.com>
2025-08-15 13:41:35 +0300

    Update sidebarsGhiduri.js

--- sidebarsGhiduri.js
+++ sidebarsGhiduri.js
@@ -28,6 +28,7 @@ export default {
                 "procesari-etl/administrative-boundaries/ro-admin-lau-line",
                 "procesari-etl/administrative-boundaries/ro-admin-county-polygon",
                 "procesari-etl/administrative-boundaries/ro-admin-county-line",
+                "procesari-etl/administrative-boundaries/ro-admin-region-polygon",
                 "procesari-etl/administrative-boundaries/ro-admin-lau-simplified-polygon",
                 "procesari-etl/administrative-boundaries/ro-admin-lau-simplified-line",
                 "procesari-etl/administrative-boundaries/ro-admin-lau-bucharest-merged-polygon",
