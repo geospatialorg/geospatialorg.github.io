2f4b1a1db74c228c5322ac5c4abae4c678dce63d
Vasile Crăciunescu <vasile.craciunescu@gmail.com>
2025-08-17 17:03:12 +0300

    Publicare macroregiuni poligon

--- sidebarsGhiduri.js
+++ sidebarsGhiduri.js
@@ -31,6 +31,7 @@ export default {
                 "procesari-etl/administrative-boundaries/ro-admin-region-polygon",
                 "procesari-etl/administrative-boundaries/ro-admin-region-line",
                 "procesari-etl/administrative-boundaries/ro-admin-macroregion-polygon",
+                "procesari-etl/administrative-boundaries/ro-admin-macroregion-line",
                 "procesari-etl/administrative-boundaries/ro-admin-lau-simplified-polygon",
                 "procesari-etl/administrative-boundaries/ro-admin-lau-simplified-line",
                 "procesari-etl/administrative-boundaries/ro-admin-lau-bucharest-merged-polygon",
