c67a01379cbdbf310ce0c6323f1e399cc74c816f
Vasile Crăciunescu <vasile.craciunescu@gmail.com>
2025-10-28 10:12:28 +0200

    Corecție link YouTube

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -175,7 +175,7 @@ const config = {
               },
               {
                 label: "YouTube",
-                href: "https://www.facebook.com/geospatialorg",
+                href: "https://www.youtube.com/@TheGeospatial",
               },
               {
                 label: "Feed RSS",
