562c3ced87e776ec855f384d1172249ad45c736c
Vasile Crăciunescu <vasile.craciunescu@gmail.com>
2026-01-23 21:23:36 +0200

    Actualizare versiune Docusaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,9 +1,4 @@
 // @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
-
 import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
@@ -14,28 +9,21 @@ const config = {
 
   markdown: {
     mermaid: true,
+    hooks: {
+      onBrokenMarkdownLinks: "warn", // mutat aici conform Docusaurus v4+
+    },
   },
 
   themes: ["@docusaurus/theme-mermaid"],
 
-  // Set the production url of your site here
-  //url: "https://geospatialorg.github.io",
   url: "https://geo-spatial.org",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "Asociația geo-spatial.org", // Usually your GitHub org/user name.
-  projectName: "geo-spatial.org", // Usually your repo name.
+  organizationName: "Asociația geo-spatial.org",
+  projectName: "geo-spatial.org",
 
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
+  onBrokenLinks: "throw", // rămâne aici
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: "en",
     locales: ["en"],
@@ -44,19 +32,15 @@ const config = {
   presets: [
     [
       "classic",
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
+      {
         docs: {
           sidebarPath: "./sidebars.js",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          //editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: false,
         theme: {
           customCss: "./src/css/custom.css",
         },
-      }),
+      },
     ],
   ],
 
@@ -86,198 +70,144 @@ const config = {
         sidebarPath: require.resolve("./sidebarsDescarcare.js"),
       },
     ],
-    // Blog 1: Noutăți
     [
       "@docusaurus/plugin-content-blog",
       {
         id: "noutati",
-        routeBasePath: "noutati", // accesezi prin /noutati
+        routeBasePath: "noutati",
         path: "./noutati",
         blogTitle: "Noutăți",
         blogDescription: "Articole de tip știri și comunicate",
       },
     ],
-
-    // Blog 2: Cunoștințe
     [
       "@docusaurus/plugin-content-blog",
       {
         id: "cunostinte",
-        routeBasePath: "cunostinte", // accesezi prin /cunostinte
+        routeBasePath: "cunostinte",
         path: "./cunostinte",
         blogTitle: "Cunoștințe",
-        blogDescription: "Articole de tip știri și comunicate",
+        blogDescription: "Articole de tip tutoriale și articole tehnice",
       },
     ],
   ],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      image: "img/geo-spatialorg-social-card.jpg",
-      metadata: [
-        // Bază
-        { name: "description", content: "An elegant place for sharing geoKnowledge & geoData" },
+  themeConfig: {
+    image: "img/geo-spatialorg-social-card.jpg",
+    metadata: [
+      { name: "description", content: "An elegant place for sharing geoKnowledge & geoData" },
+      {
+        name: "keywords",
+        content: "geo-spatial.org, GIS, teledetecție, date geospațiale, open data, cartografie, FOSS4G, OSGeo, seminarele geo-spatial.org",
+      },
+      { property: "og:type", content: "website" },
+      { property: "og:site_name", content: "geo-spatial.org" },
+      { property: "og:title", content: "An elegant place for sharing geoKnowledge & geoData" },
+      {
+        property: "og:description",
+        content: "Comunitate care promovează soluții geospațiale open-source și acces liber la date geografice.",
+      },
+      { property: "og:image", content: "https://geo-spatial.org/img/geo-spatialorg-social-card.jpg" },
+      { property: "og:url", content: "https://geo-spatial.org" },
+
+      { name: "twitter:card", content: "summary_large_image" },
+      { name: "twitter:title", content: "An elegant place for sharing geoKnowledge & geoData" },
+      {
+        name: "twitter:description",
+        content: "geo-spatial.org este o comunitate ce promovează soluții software geospațiale open source.",
+      },
+      { name: "twitter:image", content: "https://geo-spatial.org/img/geo-spatialorg-social-card.jpg" },
+      { name: "twitter:site", content: "@geospatialorg" },
+
+      { name: "discord:image", content: "https://geo-spatial.org/img/geo-spatialorg-social-card.jpg" },
+      { name: "slack-app-id", content: "geo-spatial.org" },
+    ],
+    navbar: {
+      title: "geo-spatial.org",
+      logo: {
+        alt: "Logo geo-spatial.org",
+        src: "img/logo-geo-spatialorg.svg",
+      },
+      items: [
         {
-          name: "keywords",
-          content:
-            "geo-spatial.org, GIS, teledetecție, date geospațiale, open data, date deschise, cartografie, cartografie istorică, geo, spatial, comunitate, FOSS4G, OSGeo, seminarele geo-spatial.org",
+          label: "Cunoștințe",
+          position: "left",
+          items: [
+            { to: "/cunostinte/tags/articole", label: "Articole" },
+            { to: "/cunostinte/tags/tutoriale", label: "Tutoriale" },
+            { to: "/ghiduri", label: "Ghiduri" },
+          ],
         },
-
-        // Open Graph
-        { property: "og:type", content: "website" },
-        { property: "og:site_name", content: "geo-spatial.org" },
-        { property: "og:title", content: "An elegant place for sharing geoKnowledge & geoData" },
         {
-          property: "og:description",
-          content:
-            "geo-spatial.or este o comunitate ce promovează adoptarea soluțiilor software geospațiale libere open source, neignorându-le însă nici pe cele proprietare. Militează pentru democratizarea accesului la datele geografice și propune harta ca instrument universal de comunicare și înregistrare a relațiilor dintre componentele sociale (științifice, politice, culturale, religioase, economice) și dimensiunea lor geospațială",
+          label: "Date",
+          position: "left",
+          items: [
+            { to: "https://services.geo-spatial.org/geonetwork/srv/eng/catalog.search#/search", label: "Catalog interactiv" },
+            { to: "/descarcare", label: "Descarcare directă" },
+          ],
         },
-        { property: "og:image", content: "https://geo-spatial.org/img/geo-spatialorg-social-card.jpg" },
-        { property: "og:url", content: "https://geo-spatial.org" },
-
-        // Twitter (X)
-        { name: "twitter:card", content: "summary_large_image" },
-        { name: "twitter:title", content: "An elegant place for sharing geoKnowledge & geoData" },
-        { name: "twitter:description", content: "geo-spatial.or este o comunitate ce promovează adoptarea soluțiilor software geospațiale libere open source." },
-        { name: "twitter:image", content: "https://geo-spatial.org/img/geo-spatialorg-social-card.jpg" },
-        { name: "twitter:site", content: "@geospatialorg" },
-
-        // Discord / Slack preview
-        { name: "discord:image", content: "https://geo-spatial.org/img/geo-spatialorg-social-card.jpg" },
-        { name: "slack-app-id", content: "geo-spatial.org" },
+        {
+          label: "Hărți",
+          position: "left",
+          items: [
+            { to: "https://services.geo-spatial.org/maps/#/", label: "Hărți interactive" },
+            { to: "https://geo-spatial.org/harti/#/", label: "Hărți interactive (versiunea veche)" },
+          ],
+        },
+        { to: "/noutati/tags/noutati", label: "Noutăți", position: "left" },
+        { to: "/noutati/tags/evenimente", label: "Evenimente", position: "left" },
+        {
+          href: "https://github.com/geospatialorg/geospatialorg.github.io",
+          label: "GitHub",
+          position: "right",
+        },
       ],
-      navbar: {
-        title: "geo-spatial.org",
-        logo: {
-          alt: "Logo geo-spatial.org",
-          src: "img/logo-geo-spatialorg.svg",
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Resurse",
+          items: [
+            { label: "GitHub", href: "https://github.com/geospatialorg" },
+            { label: "YouTube", href: "https://www.youtube.com/@TheGeospatial" },
+            { label: "Feed RSS", to: "https://geo-spatial.org/noutati/rss.xml" },
+          ],
         },
-        items: [
-          //  {
-          //    type: "docSidebar",
-          //    sidebarId: "tutorialSidebar",
-          //    position: "left",
-          //    label: "Tutoriale",
-          //  },
-          {
-            label: "Cunoștințe",
-            position: "left",
-            items: [
-              { to: "/cunostinte/tags/articole", label: "Articole" },
-              { to: "/cunostinte/tags/tutoriale", label: "Tutoriale" },
-              { to: "/ghiduri", label: "Ghiduri" },
-            ],
-          },
-          {
-            label: "Date",
-            position: "left",
-            items: [
-              { to: "https://services.geo-spatial.org/geonetwork/srv/eng/catalog.search#/search", label: "Catalog interactiv" },
-              { to: "/descarcare", label: "Descarcare directă" },
-            ],
-          },
-          {
-            label: "Hărți",
-            position: "left",
-            items: [
-              { to: "https://services.geo-spatial.org/maps/#/", label: "Hărți interactive" },
-              { to: "https://geo-spatial.org/harti/#/", label: "Hărți interactive (versiunea veche)" },
-            ],
-          },
-          { to: "/noutati/tags/noutati", label: "Noutăți", position: "left" },
-          { to: "/noutati/tags/evenimente", label: "Evenimente", position: "left" },
-          {
-            href: "https://github.com/geospatialorg/geospatialorg.github.io",
-            label: "GitHub",
-            position: "right",
-          },
-        ],
-      },
-      footer: {
-        style: "dark",
-        links: [
-          {
-            title: "Resurse",
-            items: [
-              {
-                label: "GitHub",
-                href: "https://github.com/geospatialorg",
-              },
-              {
-                label: "YouTube",
-                href: "https://www.youtube.com/@TheGeospatial",
-              },
-              {
-                label: "Feed RSS",
-                to: "https://geo-spatial.org/noutati/rss.xml",
-              },
-            ],
-          },
-          {
-            title: "Comunicare",
-            items: [
-              {
-                label: "Lista de discuții",
-                href: "https://groups.io/g/geo-spatial",
-              },
-              {
-                label: "Canal Slack",
-                href: "https://geospatialorg-1.slack.com/join/shared_invite/zt-cle5seo4-u9Bbr_mSV8R_BiROSGWr2A",
-              },
-            ],
-          },
-          {
-            title: "Social media",
-            items: [
-              {
-                label: "LinkedIn",
-                href: "https://www.linkedin.com/company/geo-spatial-org",
-              },
-              {
-                label: "Facebook",
-                href: "https://www.facebook.com/geospatialorg",
-              },
-              {
-                label: "Mastodon",
-                href: "https://mastodon.social/@geospatialorg",
-              },
-              {
-                label: "BlueSky",
-                href: "https://bsky.app/profile/geo-spatial-org.bsky.social",
-              },
-            ],
-          },
-          {
-            title: "Proiect",
-            items: [
-              {
-                label: "Despre",
-                to: "/despre",
-              },
-              {
-                label: "Situl vechi",
-                to: "https://www.geo-spatial.org/vechi",
-              },
-              {
-                label: "Redirecționare 3.5%",
-                href: "/redirectionare-impozit",
-              },
-              {
-                label: "Privacy",
-                href: "/privacy",
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © 2006 - 2026 geo-spatial.org și autorii. CC BY-SA 4.0`,
-      },
-      prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-        additionalLanguages: ["bash"],
-      },
-    }),
+        {
+          title: "Comunicare",
+          items: [
+            { label: "Lista de discuții", href: "https://groups.io/g/geo-spatial" },
+            { label: "Canal Slack", href: "https://geospatialorg-1.slack.com/join/shared_invite/zt-cle5seo4-u9Bbr_mSV8R_BiROSGWr2A" },
+          ],
+        },
+        {
+          title: "Social media",
+          items: [
+            { label: "LinkedIn", href: "https://www.linkedin.com/company/geo-spatial-org" },
+            { label: "Facebook", href: "https://www.facebook.com/geospatialorg" },
+            { label: "Mastodon", href: "https://mastodon.social/@geospatialorg" },
+            { label: "BlueSky", href: "https://bsky.app/profile/geo-spatial-org.bsky.social" },
+          ],
+        },
+        {
+          title: "Proiect",
+          items: [
+            { label: "Despre", to: "/despre" },
+            { label: "Situl vechi", to: "https://www.geo-spatial.org/vechi" },
+            { label: "Redirecționare 3.5%", href: "/redirectionare-impozit" },
+            { label: "Privacy", href: "/privacy" },
+          ],
+        },
+      ],
+      copyright: `Copyright © 2006 - 2026 geo-spatial.org și autorii. CC BY-SA 4.0`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+      additionalLanguages: ["bash"],
+    },
+  },
 };
 
 export default config;
